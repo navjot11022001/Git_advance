af339ab946607e4d9414c31dabda461cf3f31a1d
unknown <navjot1561.cse19@chitkara.edu.in>
2022-06-09 18:40:38 +0530

    this is my secont commit

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 console.log("this is my first comomut");
-----__________________________________
+
+
+function A() {
+    var a = 20;
+    function b() {
+        var b = 30;
+        console.log("this is a", a);
+        function c() {
+            console.log("this is b", b);
+        }
+        c();
+    }
+    b();
+
+}
+A();
