bda9818967373412183d73bf7ede9995c244ae51
unknown <navjot1561.cse19@chitkara.edu.in>
2022-06-09 18:40:38 +0530

    thi is my latest commit

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 console.log("this is my first comomut");
-----__________________________________
+
+
+function A() {
+    var a = 20;
+    function b() {
+        var b = 30;
+        console.log("this is a", a);
+        function c() {
+            console.log("this is b", b);
+        }
+        c();
+    }
+    b();
+
+}
+// A();
+
+
+function A() {
+    var a = 20;
+    for (let i = 1; i < 5; i++) {
+
+        setTimeout(() => {
+            console.log(i);
+        }, i * 1000);
+
+    }
+}
+A();
+
+
+console.log("isko kya isime rhkde");
